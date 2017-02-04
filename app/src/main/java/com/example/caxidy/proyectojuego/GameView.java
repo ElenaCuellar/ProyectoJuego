d0cd4e5a135bea483d253defbf9213e672fcd33a
ElenaCuellar <caxidy.x3@gmail.com>
2017-02-04 20:40:34 +0100

    Sound and restarting done

--- app/src/main/java/com/example/caxidy/proyectojuego/GameView.java
+++ app/src/main/java/com/example/caxidy/proyectojuego/GameView.java
@@ -1,27 +1,41 @@
 package com.example.caxidy.proyectojuego;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.support.v7.app.AlertDialog;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class GameView extends SurfaceView {
+import java.util.ArrayList;
+import java.util.List;
 
-    protected Bitmap bmp;
-    protected SurfaceHolder holder;
+public class GameView extends SurfaceView {
     protected HiloGameLoop hiloLoop;
-    protected int x = 0;
-    protected int velocidadX = 1;
+    private List<TempSprite> temps = new ArrayList<TempSprite>();
+    private List<Bola> bolas = new ArrayList<Bola>();
+    private long lastClick;
+    private Bitmap bmpExpl;
+    private SoundPool sp;
+    private int miSonido = 0;
+    public MainActivity contexto;
 
     public GameView(Context context) {
         super(context);
 
+        contexto = (MainActivity) context;
+        //Configuracion del sonido al tocar una bola
+        sp = new SoundPool(4, AudioManager.STREAM_MUSIC,0);
+        miSonido = sp.load(context,R.raw.pom,1);
+
         hiloLoop = new HiloGameLoop(this);
-        holder = getHolder();
-        holder.addCallback(new SurfaceHolder.Callback() {
+        getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
                 boolean retry = true;
@@ -30,12 +44,14 @@ public class GameView extends SurfaceView {
                     try {
                         hiloLoop.join();
                         retry = false;
-                    } catch (InterruptedException e) {}
+                    } catch (InterruptedException e) {
+                    }
                 }
             }
 
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
+                crearSprites();
                 hiloLoop.setRunning(true);
                 hiloLoop.start();
             }
@@ -43,21 +59,80 @@ public class GameView extends SurfaceView {
             public void surfaceChanged(SurfaceHolder holder, int format,
                                        int width, int height) {}
         });
-        bmp = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        bmpExpl = BitmapFactory.decodeResource(getResources(), R.drawable.explosion);
+    }
+
+    private void crearSprites() {
+        bolas.add(crearSprite(R.drawable.rojas));
+        bolas.add(crearSprite(R.drawable.azules));
+        bolas.add(crearSprite(R.drawable.verdes));
+        bolas.add(crearSprite(R.drawable.rojas));
+        bolas.add(crearSprite(R.drawable.azules));
+        bolas.add(crearSprite(R.drawable.verdes));
+    }
+
+    private Bola crearSprite(int res) {
+        Bitmap bmp = BitmapFactory.decodeResource(getResources(), res);
+        return new Bola(this,bmp);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        //Si x no se sale de la pantalla, se aumenta la posicion. Y si se sale, choca y da la vuelta
-        if (x == getWidth() - bmp.getWidth())
-            velocidadX = -1;
-
-        if (x == 0)
-            velocidadX = 1;
-
-        x += velocidadX;
-
         canvas.drawColor(Color.BLACK);
-        canvas.drawBitmap(bmp, x, 10, null);
+        for (int i = temps.size() - 1; i >= 0; i--) {
+            temps.get(i).onDraw(canvas);
+        }
+        for (Bola bola : bolas) {
+            bola.onDraw(canvas);
+        }
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (System.currentTimeMillis() - lastClick > 300) {
+            lastClick = System.currentTimeMillis();
+            float x = event.getX();
+            float y = event.getY();
+            synchronized (getHolder()) {
+                for (int i = bolas.size() - 1; i >= 0; i--) {
+                    Bola bola = bolas.get(i);
+                    if (bola.isCollition(x,y)) {
+                        bolas.remove(bola);
+                        temps.add(new TempSprite(temps, this, x, y, bmpExpl));
+                        sp.play(miSonido,1,1,1,0,1.0f);
+
+                        //sale un dialog de que has terminado el juego si ya no hay mas bolas
+                        if(bolas.size()<=0){
+                            AlertDialog.Builder alertDialogBu = new AlertDialog.Builder(contexto);
+                            alertDialogBu.setTitle(contexto.getString(R.string.titulodiag));
+                            alertDialogBu.setMessage(contexto.getString(R.string.textodiag));
+                            alertDialogBu.setIcon(R.mipmap.ic_launcher);
+                            alertDialogBu.setPositiveButton(contexto.getString(R.string.nuevoj), new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //Se vuelve a empezar el juego
+                                    crearSprites();
+                                }
+                            });
+                            alertDialogBu.setNegativeButton(contexto.getString(R.string.salir), new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    contexto.finish();
+                                }
+                            });
+
+                            AlertDialog alertDialog = alertDialogBu.create();
+                            alertDialog.setCanceledOnTouchOutside(false);
+                            alertDialog.show();
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    protected void cerrarJuego(){
+        sp.release();
+    }
+
 }
--- app/src/main/java/com/example/caxidy/proyectojuego/MainActivity.java
+++ app/src/main/java/com/example/caxidy/proyectojuego/MainActivity.java
@@ -6,9 +6,25 @@ import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
+    GameView gameView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new GameView(this));
+        gameView=new GameView(this);
+        setContentView(gameView);
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //!!aqui para guardar las bolas y posiciones en sharedprefs...
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        gameView.cerrarJuego();
+    }
+
 }
